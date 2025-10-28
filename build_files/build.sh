a76d6eeecb5888fe95be30fb4a24add27cb44ac2
Rufo Sanchez <rufo@rufosanchez.com>
2025-10-27 21:54:49 -0500

    First attempt at customizations

--- build_files/build.sh
+++ build_files/build.sh
@@ -15,6 +15,14 @@ cp -avf "/ctx/system_files"/. /
 # this installs a package from fedora repos
 dnf5 install -y tmux
 
+rpm --import https://downloads.1password.com/linux/keys/1password.asc
+sh -c 'echo -e "[1password]\nname=1Password Stable Channel\nbaseurl=https://downloads.1password.com/linux/rpm/stable/\$basearch\nenabled=1\ngpgcheck=1\nrepo_gpgcheck=1\ngpgkey=\"https://downloads.1password.com/linux/keys/1password.asc\"" > /etc/yum.repos.d/1password.repo'
+dnf5 install -y 1password
+
+curl -fsSL https://repo.ivpn.net/stable/fedora/generic/ivpn.repo > /etc/yum.repos.d/ivpn.repo
+dnf5 install -y ivpn ivpn-ui
+
+
 # Use a COPR Example:
 #
 # dnf5 -y copr enable ublue-os/staging
