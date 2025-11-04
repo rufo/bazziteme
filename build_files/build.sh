d431f903d3d0d618889da637d86f860c3cb5fc56
Rufo Sanchez <rufo@rufosanchez.com>
2025-11-04 09:25:32 -0600

    Switch /opt to "supported" workaround

--- build_files/build.sh
+++ build_files/build.sh
@@ -12,11 +12,6 @@ set -ouex pipefail
 # this installs a package from fedora repos
 dnf5 install -y tmux
 
-# fix /opt
-# from https://universal-blue.discourse.group/t/opt-build-error-fix-for-custom-images/10706
-rm /opt # this was a symlink file not a directory
-mkdir /opt # recreate the opt directory to enable packages to install in it
-
 rpm --import https://downloads.1password.com/linux/keys/1password.asc
 sh -c 'echo -e "[1password]\nname=1Password Stable Channel\nbaseurl=https://downloads.1password.com/linux/rpm/stable/\$basearch\nenabled=1\ngpgcheck=1\nrepo_gpgcheck=1\ngpgkey=\"https://downloads.1password.com/linux/keys/1password.asc\"" > /etc/yum.repos.d/1password.repo'
 dnf5 install -y 1password
