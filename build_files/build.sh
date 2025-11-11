6d261254a25f06abc536115a10016354a479d5d3
Rufo Sanchez <rufo@rufosanchez.com>
2025-11-11 17:11:44 -0600

    Disable 1Password installation in build script
    Comment out 1Password installation commands in build.sh

--- build_files/build.sh
+++ build_files/build.sh
@@ -12,9 +12,9 @@ cp -avf "/ctx/system_files"/. /
 # List of rpmfusion packages can be found here:
 # https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/43/x86_64/repoview/index.html&protocol=https&redirect=1
 
-rpm --import https://downloads.1password.com/linux/keys/1password.asc
-sh -c 'echo -e "[1password]\nname=1Password Stable Channel\nbaseurl=https://downloads.1password.com/linux/rpm/stable/\$basearch\nenabled=1\ngpgcheck=1\nrepo_gpgcheck=1\ngpgkey=\"https://downloads.1password.com/linux/keys/1password.asc\"" > /etc/yum.repos.d/1password.repo'
-dnf5 install -y 1password
+# rpm --import https://downloads.1password.com/linux/keys/1password.asc
+# sh -c 'echo -e "[1password]\nname=1Password Stable Channel\nbaseurl=https://downloads.1password.com/linux/rpm/stable/\$basearch\nenabled=1\ngpgcheck=1\nrepo_gpgcheck=1\ngpgkey=\"https://downloads.1password.com/linux/keys/1password.asc\"" > /etc/yum.repos.d/1password.repo'
+# dnf5 install -y 1password
 
 curl -fsSL https://repo.ivpn.net/stable/fedora/generic/ivpn.repo > /etc/yum.repos.d/ivpn.repo
 dnf5 install -y ivpn ivpn-ui
